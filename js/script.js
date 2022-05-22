a4adbfbd435e2a29f2c3b19c369b2b0c61526e52
augustin-marie <a.marie189@laposte.net>
2022-05-22 18:42:28 +0200

    Changement des path de toutes mes intégration pour correspondre a la structure imposée par github pages

--- js/script.js
+++ js/script.js
@@ -1,28 +1,26 @@
-window.onload = () => {
-    /* Fonctions d'initialisation
-    ----------------------------------------*/
-    function init(){
-        loadPages("/html/home.html")
-    }
-
-    /*  Fonction du chargement des pages
-    ----------------------------------------*/
-    $("a").click(function (e) { 
-        e.preventDefault();
-
-        contentPath = e.target.pathname;
-
-        if(contentPath !== ""){
-            loadPages(contentPath)
-        }
-    });
-
-
-    function loadPages(contentPath) {
-        $('#container').load(contentPath);
-    }
-
-    /* Initialisation
-    ----------------------------------------*/
-    init()
+/* Fonctions d'initialisation
+----------------------------------------*/
+function init(){
+    loadPages("../html/home.html")
 }
+
+/*  Fonction du chargement des pages
+----------------------------------------*/
+$("a").click(function (e) { 
+    e.preventDefault();
+
+    contentPath = e.target.pathname;
+
+    if(contentPath !== ""){
+        loadPages(contentPath)
+    }
+});
+
+
+function loadPages(contentPath) {
+    $('#container').load(contentPath);
+}
+
+/* Initialisation
+----------------------------------------*/
+init()
