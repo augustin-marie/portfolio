6b010cd0f73229110039fe009f4954ea8fced1ae
augustin-marie <a.marie189@laposte.net>
2022-05-22 18:23:52 +0200

    le script s'execute après le chargement complet de la page

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-/* Fonctions d'initialisation
-----------------------------------------*/
-function init(){
-    loadPages("/html/home.html")
-}
-
-/*  Fonction du chargement des pages
-----------------------------------------*/
-$("a").click(function (e) { 
-    e.preventDefault();
-
-    contentPath = e.target.pathname;
-
-    if(contentPath !== ""){
-        loadPages(contentPath)
+window.onload = () => {
+    /* Fonctions d'initialisation
+    ----------------------------------------*/
+    function init(){
+        loadPages("/html/home.html")
     }
-});
+
+    /*  Fonction du chargement des pages
+    ----------------------------------------*/
+    $("a").click(function (e) { 
+        e.preventDefault();
+
+        contentPath = e.target.pathname;
+
+        if(contentPath !== ""){
+            loadPages(contentPath)
+        }
+    });
 
 
-function loadPages(contentPath) {
-    $('#container').load(contentPath);
+    function loadPages(contentPath) {
+        $('#container').load(contentPath);
+    }
+
+    /* Initialisation
+    ----------------------------------------*/
+    init()
 }
-
-/* Initialisation
-----------------------------------------*/
-init()
